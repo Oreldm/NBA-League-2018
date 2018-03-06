b4b1447f3416277120068f203117b15878107b66
ariel <37008455+arielsgl@users.noreply.github.com>
2018-03-06 15:25:24 +0200

    check

--- sql_basketball/src/Panels/Remove_Player_Panel.java
+++ sql_basketball/src/Panels/Remove_Player_Panel.java
@@ -18,35 +18,26 @@ public class Remove_Player_Panel extends JPanel{
 	private String ADMIN = "Administrator";
 	
 	public Remove_Player_Panel() {
-//		final JPanel panel = new JPanel();
-//		final String premissions[] = {GUEST, ADMIN};
-//		final JScrollPane scroll = new JScrollPane(panel);
-//		final JButton jbInsertUser = new JButton("Insert User");
-//		panel.setBorder(BorderFactory.createLineBorder(Color.red));
-//		panel.setLayout(new GridLayout(3, 2, 2, 2));
-//		JLabel username = new JLabel("Username:");
-//		JTextField jtfUsername = new JTextField();
-//		JLabel password = new JLabel("Password:");
-//		JTextField jtfPassword = new JTextField();
-//		JLabel premission = new JLabel("Premission:");
-//		JComboBox<String> jCPremission = new JComboBox<>(premissions);
-//		panel.add(username);
-//		panel.add(jtfUsername);
-//		panel.add(password);
-//		panel.add(jtfPassword);
-//		panel.add(premission);
-//		panel.add(jCPremission);
-//		setLayout(new BorderLayout());
-//		add(jbInsertUser, BorderLayout.SOUTH);
-//		add(scroll, BorderLayout.CENTER);
-//		setSize(300, 300);
-//		setVisible(true);
+		final JPanel panel = new JPanel();
+		final JScrollPane scroll = new JScrollPane(panel);
+		final JButton jbDeletePlayer = new JButton("Delete Player");
+		panel.setBorder(BorderFactory.createLineBorder(Color.red));
+		panel.setLayout(new GridLayout(1, 2, 2, 2));
+		JLabel playerId = new JLabel("Player ID:");
+		JTextField jtfUsername = new JTextField();
+		panel.add(playerId);
+		panel.add(jtfUsername);
+		setLayout(new BorderLayout());
+		add(jbDeletePlayer, BorderLayout.SOUTH);
+		add(scroll, BorderLayout.CENTER);
+		setSize(300, 300);
+		setVisible(true);
 	}
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		frame.add(new Remove_Player_Panel());
 		frame.pack();
-		frame.setSize(300, 180);
+		frame.setSize(300, 100);
 		frame.setLocationRelativeTo(null); // Center the frame
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setAlwaysOnTop(true);
