801e5cb16c36350ec92a22ba785555d66b7431a9
ariel <37008455+arielsgl@users.noreply.github.com>
2018-03-06 15:12:06 +0200

    asd

--- sql_basketball/src/Panels/Remove_Player_Panel.java
+++ sql_basketball/src/Panels/Remove_Player_Panel.java
@@ -13,38 +13,38 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
-public class Remove_Player_Panel {
+public class Remove_Player_Panel extends JPanel{
 	private String GUEST = "Guest";
 	private String ADMIN = "Administrator";
 	
 	public Remove_Player_Panel() {
-		final JPanel panel = new JPanel();
-		final String premissions[] = {GUEST, ADMIN};
-		final JScrollPane scroll = new JScrollPane(panel);
-		final JButton jbInsertUser = new JButton("Insert User");
-		panel.setBorder(BorderFactory.createLineBorder(Color.red));
-		panel.setLayout(new GridLayout(3, 2, 2, 2));
-		JLabel username = new JLabel("Username:");
-		JTextField jtfUsername = new JTextField();
-		JLabel password = new JLabel("Password:");
-		JTextField jtfPassword = new JTextField();
-		JLabel premission = new JLabel("Premission:");
-		JComboBox<String> jCPremission = new JComboBox<>(premissions);
-		panel.add(username);
-		panel.add(jtfUsername);
-		panel.add(password);
-		panel.add(jtfPassword);
-		panel.add(premission);
-		panel.add(jCPremission);
-		setLayout(new BorderLayout());
-		add(jbInsertUser, BorderLayout.SOUTH);
-		add(scroll, BorderLayout.CENTER);
-		setSize(300, 300);
-		setVisible(true);
+//		final JPanel panel = new JPanel();
+//		final String premissions[] = {GUEST, ADMIN};
+//		final JScrollPane scroll = new JScrollPane(panel);
+//		final JButton jbInsertUser = new JButton("Insert User");
+//		panel.setBorder(BorderFactory.createLineBorder(Color.red));
+//		panel.setLayout(new GridLayout(3, 2, 2, 2));
+//		JLabel username = new JLabel("Username:");
+//		JTextField jtfUsername = new JTextField();
+//		JLabel password = new JLabel("Password:");
+//		JTextField jtfPassword = new JTextField();
+//		JLabel premission = new JLabel("Premission:");
+//		JComboBox<String> jCPremission = new JComboBox<>(premissions);
+//		panel.add(username);
+//		panel.add(jtfUsername);
+//		panel.add(password);
+//		panel.add(jtfPassword);
+//		panel.add(premission);
+//		panel.add(jCPremission);
+//		setLayout(new BorderLayout());
+//		add(jbInsertUser, BorderLayout.SOUTH);
+//		add(scroll, BorderLayout.CENTER);
+//		setSize(300, 300);
+//		setVisible(true);
 	}
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
-		frame.add(new Add_USER_Panel());
+		frame.add(new Remove_Player_Panel());
 		frame.pack();
 		frame.setSize(300, 180);
 		frame.setLocationRelativeTo(null); // Center the frame
